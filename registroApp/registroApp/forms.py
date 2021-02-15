87087e1d294e6a4c8c626f72b84f575f184164f4
Cupul <carlos.cupul02@gmail.com>
2021-02-15 12:10:45 -0600

    se añadio un archivo de formulario para el login y registro de los usuarios

--- registroApp/registroApp/forms.py
+++ registroApp/registroApp/forms.py
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
 import re
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
-
 
 class LoginForm(AuthenticationForm):
 
@@ -20,7 +18,6 @@ class LoginForm(AuthenticationForm):
                 classes = self.fields[f_name].widget.attrs.get('class', '')
                 classes += ' has-error'
                 self.fields[f_name].widget.attrs['class'] = classes
-
 
 class RegistrationForm(forms.Form):
 
